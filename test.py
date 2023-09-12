132faea3dedcf0bcb696857a17c702c5f5fdd53c
SujjithS <81348122+SujjithS@users.noreply.github.com>
2023-09-12 09:58:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--shutdown')
+parser.add_argument("shutdown", help="display a sensor stater",
+                    type=string)
 args = parser.parse_args()
-
-
 print(args.shutdown)
