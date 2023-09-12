a69796ec8df0ef5fb6906175f93dfa350d06e241
SujjithS <81348122+SujjithS@users.noreply.github.com>
2023-09-12 10:09:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("shutdown")
+parser.add_argument("shutdown", action='store_true')
+parser.add_argument("start", action='store_true')
 args = parser.parse_args()
-print(args.shutdown)
+
+if args.shutdown and not args.start:
+  print("The sensor will be shutdown")
+
+elif not args.shutdown and args.start:
+  print("The sensor will be shutdown")
+  
+else:
+  parser.error("No argument set or too many. You have to set one argument")
